0888ec8894886b8305fb22cae7e9dbe537d180af
Peng Chen <chenpeng1221@outlook.com>
2023-03-24 14:35:17 +0100

    create files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 require("./config")(app);
 
 const capitalize = require("./utils/capitalize");
-const projectName = "myproject";
+const projectName = "pokemon-gogogo";
 
 app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
